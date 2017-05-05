88f56850263ad486cb0a1d3d47f7c48807e4d29e
Armaan Dhir <dhir.armaan@gmail.com>
2017-05-05 03:19:32 -0400

    Initial commit
    Project created

---
+++ src/main/java/com/armaandhir/qlik/QlikAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.armaandhir.qlik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlikAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlikAssignmentApplication.class, args);
+	}
+}
